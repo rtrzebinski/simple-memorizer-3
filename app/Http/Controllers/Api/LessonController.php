686bcb0e41ef51e8de534cf018463a8e943c26da
Robert Trzebinski <robert@trzebinski.info>
2016-06-11 12:45:31 +0100

    empty LessonController

---
+++ app/Http/Controllers/Api/LessonController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+class LessonController
+{
+    public function listLessons()
+    {
+
+    }
+
+    public function retrieveLesson($id)
+    {
+
+    }
+
+    public function storeLesson()
+    {
+
+    }
+
+    public function updateLesson()
+    {
+
+    }
+
+    public function deleteLesson()
+    {
+
+    }
+}
