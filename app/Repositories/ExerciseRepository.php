4889eadc9ae0d3192b34ef20cedc4f241656b289
Robert Trzebinski <robert@trzebinski.info>
2016-06-14 19:22:58 +0100

    implement ExerciseController

--- app/Repositories/ExerciseRepository.php
+++ app/Repositories/ExerciseRepository.php
@@ -9,26 +9,33 @@ class ExerciseRepository
 {
     public function fetchExercisesOfUser(int $userId) : Collection
     {
-
+        return Exercise::whereUserId($userId)->get();
     }
 
     public function findExerciseById(int $exerciseId) : Exercise
     {
-
+        return Exercise::findOrFail($exerciseId);
     }
 
     public function createExercise(int $userId, array $input) : Exercise
     {
-
+        $exercise = new Exercise($input);
+        $exercise->user_id = $userId;
+        $exercise->save();
+        return $exercise;
     }
 
     public function updateExercise(int $exerciseId, array $input) : Exercise
     {
-
+        $exercise = $this->findExerciseById($exerciseId);
+        $exercise->fill($input);
+        $exercise->save();
+        return $exercise;
     }
 
     public function deleteExercise(int $exerciseId)
     {
-
+        $exercise = $this->findExerciseById($exerciseId);
+        $exercise->delete();
     }
 }
---
+++ tests/Http/Repositories/ExerciseRepositoryTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Http\Repositories;
+
+use App\Exercise;
+use App\Repositories\ExerciseRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use TestCase;
+
+class ExerciseRepositoryTest extends TestCase
+{
+    /**
+     * @var ExerciseRepository
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->repository = new ExerciseRepository();
+    }
+
+    public function testItShould_fetchExercisesOfUser()
+    {
+        $user = $this->createUser();
+        $exercise = $this->createExercise(['user_id' => $user->id]);
+
+        $result = $this->repository->fetchExercisesOfUser($user->id);
+
+        $this->assertCount(1, $result);
+        $this->assertEquals($exercise->id, $result[0]->id);
+    }
+
+    public function testItShould_findExerciseById()
+    {
+        $exercise = $this->createExercise();
+        $this->assertEquals($exercise->id, $this->repository->findExerciseById($exercise->id)->id);
+    }
+
+    public function testItShould_notFindExerciseById_exerciseDoesNotExist()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->findExerciseById(-1);
+    }
+
+    public function testItShould_createExercise()
+    {
+        $user = $this->createUser();
+        $question = uniqid();
+        $answer = uniqid();
+
+        $exercise = $this->repository->createExercise($user->id, [
+            'question' => $question,
+            'answer' => $answer,
+        ]);
+
+        $this->assertInstanceOf(Exercise::class, $exercise);
+        $this->assertEquals($question, $exercise->question);
+        $this->assertEquals($answer, $exercise->answer);
+        $this->assertEquals($user->id, $exercise->user_id);
+    }
+
+    public function testItShould_updateExercise()
+    {
+        $exercise = $this->createExercise();
+        $question = uniqid();
+        $answer = uniqid();
+
+        $exercise = $this->repository->updateExercise($exercise->id, [
+            'question' => $question,
+            'answer' => $answer,
+        ])->fresh();
+
+        $this->assertInstanceOf(Exercise::class, $exercise);
+        $this->assertEquals($question, $exercise->question);
+        $this->assertEquals($answer, $exercise->answer);
+    }
+
+    public function testItShould_notUpdateExercise_exerciseDoesNotExist()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->updateExercise(-1, []);
+    }
+
+    public function testItShould_deleteExercise()
+    {
+        $exercise = $this->createExercise();
+
+        $this->repository->deleteExercise($exercise->id);
+
+        $this->assertNull($exercise->fresh());
+    }
+
+    public function testItShould_notDeleteExercise_exerciseDoesNotExist()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->deleteExercise(-1);
+    }
+}
