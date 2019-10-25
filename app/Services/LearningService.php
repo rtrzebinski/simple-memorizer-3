f193a359afa93040ad8f2dabf366d83439fa545f
Robert Trzebinski <robert@cornerstone.co.uk>
2019-10-25 17:27:25 +0100

    change learning points range from 1-10 to 1-100

--- app/Services/LearningService.php
+++ app/Services/LearningService.php
@@ -79,47 +79,16 @@ class LearningService
     }
 
     /**
-     * Calculate number of points
-     *
-     * 1 means highest familiarity with the answer.
-     * 10 means lowest familiarity with the answer.
-     *
      * @param int $percentOfGoodAnswers
      * @return int
      * @throws \Exception
      */
     private function calculateNumberOfPoints(int $percentOfGoodAnswers): int
     {
-        if ($percentOfGoodAnswers <= 100 && $percentOfGoodAnswers > 90) {
+        if ($percentOfGoodAnswers == 100) {
             return 1;
         }
-        if ($percentOfGoodAnswers <= 90 && $percentOfGoodAnswers > 80) {
-            return 2;
-        }
-        if ($percentOfGoodAnswers <= 80 && $percentOfGoodAnswers > 70) {
-            return 3;
-        }
-        if ($percentOfGoodAnswers <= 70 && $percentOfGoodAnswers > 60) {
-            return 4;
-        }
-        if ($percentOfGoodAnswers <= 60 && $percentOfGoodAnswers > 50) {
-            return 5;
-        }
-        if ($percentOfGoodAnswers <= 50 && $percentOfGoodAnswers > 40) {
-            return 6;
-        }
-        if ($percentOfGoodAnswers <= 40 && $percentOfGoodAnswers > 30) {
-            return 7;
-        }
-        if ($percentOfGoodAnswers <= 30 && $percentOfGoodAnswers > 20) {
-            return 8;
-        }
-        if ($percentOfGoodAnswers <= 20 && $percentOfGoodAnswers > 10) {
-            return 9;
-        }
-        if ($percentOfGoodAnswers <= 10 && $percentOfGoodAnswers >= 0) {
-            return 10;
-        }
-        throw new Exception('$percentOfGoodAnswers must be a value between 0 and 100');
+
+        return 100 - $percentOfGoodAnswers;
     }
 }
