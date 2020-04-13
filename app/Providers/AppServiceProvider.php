50d2475e451ba7e1839ad5f64dc5c5d3bd30eed2
Robert Trzebinski <robert@trzebinski.info>
2020-04-13 09:06:33 +0200

    move structures interfaces bindings to dedicated StructuresServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,6 @@
 
 namespace App\Providers;
 
-use App\Structures\UserExercise\AuthenticatedUserExerciseRepository;
-use App\Structures\UserExercise\AuthenticatedUserExerciseRepositoryInterface;
-use App\Structures\UserLesson\AuthenticatedUserLessonRepository;
-use App\Structures\UserLesson\AuthenticatedUserLessonRepositoryInterface;
-use App\Structures\UserLesson\GuestUserLessonRepository;
-use App\Structures\UserLesson\GuestUserLessonRepositoryInterface;
-use App\Structures\UserExercise\GuestUserExerciseRepository;
-use App\Structures\UserExercise\UserExerciseRepositoryInterface;
-use App\Structures\UserLesson\UserLessonRepositoryInterface;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -38,36 +28,5 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() === 'production') {
             URL::forceScheme('https');
         }
-
-        // UserExercise operations valid for all (authenticated and guest) users
-        $this->app->bind(UserExerciseRepositoryInterface::class, function () {
-            if (Auth::check()) {
-                return new AuthenticatedUserExerciseRepository(Auth::user());
-            }
-            return new GuestUserExerciseRepository();
-        });
-
-        // UserExercise operations valid for authenticated users only
-        $this->app->bind(AuthenticatedUserExerciseRepositoryInterface::class, function () {
-            return new AuthenticatedUserExerciseRepository(Auth::user());
-        });
-
-        // UserLesson operations valid for all (authenticated and guest) users
-        $this->app->bind(UserLessonRepositoryInterface::class, function () {
-            if (Auth::check()) {
-                return new AuthenticatedUserLessonRepository(Auth::user());
-            }
-            return new GuestUserLessonRepository();
-        });
-
-        // UserLesson operations valid for authenticated users only
-        $this->app->bind(AuthenticatedUserLessonRepositoryInterface::class, function () {
-            return new AuthenticatedUserLessonRepository(Auth::user());
-        });
-
-        // UserLesson operations valid for guest users only
-        $this->app->bind(GuestUserLessonRepositoryInterface::class, function () {
-            return new GuestUserLessonRepository();
-        });
     }
 }
--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\StructuresServiceProvider::class,
 
     ],
 
